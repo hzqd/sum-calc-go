9c3e54c0a7f92db1e09fe51bb566dbf943b63835
hzqd <hzqelf@yeah.net>
2023-12-11 23:00:30 +0800

    Update future.go

--- ext/future.go
+++ ext/future.go
@@ -2,7 +2,6 @@ package ext
 
 import (
 	"sync"
-	"sync/atomic"
 )
 
 type Future[T any] struct {
@@ -10,38 +9,30 @@ type Future[T any] struct {
 }
 
 type future[T any] struct {
-	runFn   func(func(T))
-	wait    sync.WaitGroup
-	pending atomic.Bool
-	result  T
+	runFn  func() T
+	wait   sync.WaitGroup
+	result T
 }
 
-func Async[T any](runFn func(resolve func(T))) Future[T] {
+func Spawn[T any](runFn func() T) Future[T] {
 	f := Future[T]{&future[T]{
-		runFn:   runFn,
-		wait:    sync.WaitGroup{},
-		pending: atomic.Bool{},
+		runFn: runFn,
+		wait:  sync.WaitGroup{},
 	}}
 	f.wait.Add(1)
-	f.pending.Store(true)
-	go f.runFn(f.resolve)
+	go func() {
+		f.result = f.runFn()
+		f.runFn = nil
+		f.wait.Done()
+	}()
 	return f
 }
 
 func (f Future[T]) Await() T {
-	if f.pending.Load() {
-		f.wait.Wait()
-	}
+	f.wait.Wait()
 	return f.result
 }
 
 func (f Future[T]) TryGet() (T, bool) {
-	return f.result, !f.pending.Load()
-}
-
-func (f Future[T]) resolve(value T) {
-	if f.pending.Load() {
-		f.result = value
-		f.wait.Done()
-	}
+	return f.result, f.runFn == nil
 }
